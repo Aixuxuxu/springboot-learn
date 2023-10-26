f154a7811916ce089ca184b02233b839b5b5d4bf
被风吹散的路边摊 <3085889216@qq.com>
2023-10-26 10:56:54 +0800

    fix

--- aixu-springboot-redis/src/test/java/com/aixu/redisTest.java
+++ aixu-springboot-redis/src/test/java/com/aixu/redisTest.java
@@ -3,7 +3,6 @@ package com.aixu;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
